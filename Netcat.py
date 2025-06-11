72e39bece295955c80afc810e301ded8a02123c2
ghost <ghost@ghost.ghost>
2025-06-11 04:41:08 -0400

    initial project

--- Netcat.py
+++ Netcat.py
@@ -1,14 +1,34 @@
 import socket
 import sys
 import threading
-import index as execute
 
 
-class Netcat :
+class Netcat:
+    
+    def __init__(self, args, buffer):
+        self.buffer = buffer
+        self.args = args
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    
+    def execute(self, cmd):
+        """Execute a command and return output"""
+        import subprocess
+        import shlex
+        
+        cmd = cmd.strip()
+        if not cmd:
+            return ""
+        
+        try:
+            output = subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDOUT)
+            return output.decode()
+        except Exception as e:
+            return f"Error executing command: {e}\n"
     
     def handle_client(self, client_socket):
         if self.args.execute:
-            output = execute(self.args.execute)
+            output = self.execute(self.args.execute)
             client_socket.send(output.encode())
         elif self.args.upload:
             file_buffer = b''
@@ -17,26 +37,38 @@ class Netcat :
                 if not data:
                     break
                 file_buffer += data
-            with open(self.args.upload, 'wb') as f:
-                f.write(file_buffer)
-            client_socket.send(f'Saved file to {self.args.upload}\n'.encode())
+            
+            try:
+                with open(self.args.upload, 'wb') as f:
+                    f.write(file_buffer)
+                client_socket.send(f'Saved file to {self.args.upload}\n'.encode())
+            except Exception as e:
+                client_socket.send(f'Error saving file: {e}\n'.encode())
+                
         elif self.args.command:
             while True:
                 try:
-                    client_socket.send(b'Netcat> dev by Backtrack\n from Terfi Mohammed Wassim \n')
+                    client_socket.send(b'Netcat> dev by Backtrack\nfrom Terfi Mohammed Wassim\n')
                     client_socket.send(b'Enter command: ')
                     cmd_buffer = b''
                     while b'\n' not in cmd_buffer:
-                        cmd_buffer += client_socket.recv(64)
-                    response = execute(cmd_buffer.decode())
+                        data = client_socket.recv(64)
+                        if not data:
+                            break
+                        cmd_buffer += data
+                    
+                    if not cmd_buffer:
+                        break
+                        
+                    response = self.execute(cmd_buffer.decode().strip())
                     if response:
                         client_socket.send(response.encode())
-                    cmd_buffer = b''
                 except Exception as e:
                     print(f'Error: {e}')
                     client_socket.send(b'Error executing command.\n')
-                    client_socket.close()
-                    sys.exit(0)
+                    break
+        
+        client_socket.close()
                     
     def listen(self):
         self.socket.bind((self.args.target, self.args.port))
@@ -44,44 +76,41 @@ class Netcat :
         print(f'Listening on {self.args.target}:{self.args.port}')
         
         while True:
-            client_socket, addr = self.socket.accept()
-            print(f'Connection from {addr}')
-            client_handler = threading.Thread(target=self.handle_client, args=(client_socket,))
-            client_handler.start()
-       
-       
-       
+            try:
+                client_socket, addr = self.socket.accept()
+                print(f'Connection from {addr}')
+                client_handler = threading.Thread(target=self.handle_client, args=(client_socket,))
+                client_handler.daemon = True
+                client_handler.start()
+            except KeyboardInterrupt:
+                print("\nShutting down server...")
+                break
+        
+        self.socket.close()
+    
     def send(self):
-        self.socket.connect((self.args.target, self.args.port))
-        if self.buffer:
-            self.socket.send(self.buffer)
         try:
+            self.socket.connect((self.args.target, self.args.port))
+            if self.buffer:
+                self.socket.send(self.buffer)
+            
             while True:
                 response = self.socket.recv(4096)
                 if not response:
                     break
                 sys.stdout.write(response.decode())
-                sys.stdout.flush()        
-            self.socket.close()
-        
+                sys.stdout.flush()
+                
         except KeyboardInterrupt:
             print("\nConnection closed by user.")
+        except Exception as e:
+            print(f"Connection error: {e}")
+        finally:
             self.socket.close()
-            sys.exit(0)
-            
-            
     
-    def __init__(self,args,buffer):
-        self.buffer = buffer
-        self.args = args
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        
-        
     def run(self):
         if self.args.listen:
             self.listen()
         else:
             self.send()
-        
-        
+
--- index.py
+++ index.py
@@ -1,10 +1,9 @@
-
 import argparse
 import textwrap
 import shlex
 import subprocess
 import sys
-import Netcat
+from Netcat import Netcat  
 
 def execute(cmd):
     cmd = cmd.strip()
@@ -20,34 +19,34 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Backtrack tool Netcat Clone',
         formatter_class = argparse.RawDescriptionHelpFormatter,
-        epilog=textwrap.dedent('''Example: 2
-                        Netcat.py -t 192.168.1.108 -p 5555 -l -c #
-                        command shell
-            Netcat.py -t 192.168.1.108 -p 5555 -l -
-u=mytest.txt # upload to file
-Netcat.py -t 192.168.1.108 -p 5555 -l -e=\"cat
-/etc/passwd\" # execute command
-echo 'ABC' | ./Netcat.py -t 192.168.1.108 -p 135
-# echo text to server port 135
-Netcat.py -t 192.168.1.108 -p 5555 # connect to
-server
+        epilog=textwrap.dedent('''Examples:
+                        python index.py -t 192.168.1.108 -p 5555 -l -c
+                        # command shell
+                        python index.py -t 192.168.1.108 -p 5555 -l -u=mytest.txt
+                        # upload to file
+                        python index.py -t 192.168.1.108 -p 5555 -l -e="cat /etc/passwd"
+                        # execute command
+                        echo 'ABC' | python index.py -t 192.168.1.108 -p 135
+                        # echo text to server port 135
+                        python index.py -t 192.168.1.108 -p 5555
+                        # connect to server
 ''')
     )
     
-    parser.add_argument('-c','--command',action='store_true',help='listin')
-    parser.add_argument('-e','--execute',help='specified command')
-    parser.add_argument('-l','--listin')
+    parser.add_argument('-c','--command',action='store_true',help='enable command shell')
+    parser.add_argument('-e','--execute',help='execute specified command')
+    parser.add_argument('-l','--listen',action='store_true',help='listen mode')  # Fixed typo
     parser.add_argument('-p','--port',type=int,default=5555,help='specified port')
     parser.add_argument('-t','--target',default='192.168.0.120',help='specified ip')
     parser.add_argument('-u','--upload',help='upload a file')
     
     args = parser.parse_args()
 
-    if args.listin:
+    if args.listen:  
         buffer = ''
     else : 
         buffer = sys.stdin.read()
     
     nc = Netcat(args,buffer.encode())
     nc.run()
-    
+
